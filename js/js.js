0f9e83044e6f83738974a822cc25b93a113832c0
Sol Kim <solkim813@gmail.com>
2017-04-19 14:37:16 -0400

    more drawing updates

--- js/js.js
+++ js/js.js
@@ -12,21 +12,50 @@ function resizeCanvas() {
 		 * Your drawings need to be inside this function otherwise they will be reset when 
 		 * you resize the browser window and the canvas goes will be cleared.
 		 */
-		drawStuff(); 
+		draw1(); 
 }
 resizeCanvas();
 
-function drawStuff() {
+function draw1() {
+	var centerX = canvas.width / 2;
+	var centerY = canvas.height / 2;
+	var radius = 40;
+	
+	var coords = [ [centerX-60,centerY-60], [centerX+60,centerY+60], [centerX-60,centerY+60] ];
+	ctx.fillStyle = '#b0e4ff';
+	for(var i = 0; i < coords.length; i++){
+		ctx.beginPath();
+		ctx.arc(coords[i][0], coords[i][1], radius, 0, Math.PI * 2, false);
+		ctx.fill();
+	}
+	
 	ctx.beginPath();
-	for (i = 20; i < canvas.height; i += 50) {
-		ctx.moveTo(0, i);
-		ctx.lineTo(canvas.width, i);
-	}
-	for (i = 20; i < canvas.width; i += 50) {
-		ctx.moveTo(i, 0);
-		ctx.lineTo(i, canvas.height);
-	}
-	ctx.lineWidth = 4;
-	ctx.strokeStyle = '#fff';
-	ctx.stroke();
+		ctx.arc(centerX+60, centerY-60, 47, 0, Math.PI * 2, false);
+		ctx.fill();
 }
+
+//function draw1() {
+//	ctx.beginPath();
+//	// regular horizontal lines
+//	for (i = 20; i < 230; i += 70) {
+//		ctx.moveTo(0, i);
+//		ctx.lineTo(canvas.width, i);
+//	}
+//	// skewed horizontal line
+//	ctx.moveTo(0, 230);
+//	ctx.lineTo(canvas.width, 240);
+//	// regular horizontal lines
+//	for (i = 300; i < canvas.height; i += 70) {
+//		ctx.moveTo(0, i);
+//		ctx.lineTo(canvas.width, i);
+//	}
+//	
+//	// regular vertical lines
+//	for (i = 20; i < canvas.width; i += 70) {
+//		ctx.moveTo(i, 0);
+//		ctx.lineTo(i, canvas.height);
+//	}
+//	ctx.lineWidth = 6;
+//	ctx.strokeStyle = '#000';
+//	ctx.stroke();
+//}
